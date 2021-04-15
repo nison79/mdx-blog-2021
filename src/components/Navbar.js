54398e998b90d73bbc57cd4a7ca3a74636c4028e
nison79 <georgenison@gmail.com>
2021-04-15 13:05:07 +0300

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { FaBars } from 'react-icons/fa'
+import logo from '../assets/logo.svg'
+import Links from '../constants/links'
+import SocialLinks from '../constants/socialLinks'
+import { FiLayout } from 'react-icons/fi'
+
+
+const Navbar = ({ toggle }) => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/" className="nav-logo">
+            <img src={logo} alt="mdx-logo"></img>
+          </Link>
+          <button className="toggle-btn" onClick={toggle}>
+            <FaBars />
+          </button>
+        </div>
+        <Links styleClass="nav-links" />
+        <SocialLinks styleClass="nav-icons" />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
